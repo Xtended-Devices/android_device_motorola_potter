c6e693c6397acfc021a32ca4ad7410aa6bc2c073
Anish Pratheepan <42488259+ultranoob-5@users.noreply.github.com>
2019-08-25 19:43:50 +0530

    potter: remove SD Clang and Remove GCC flag path

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -83,9 +83,7 @@ TARGET_KERNEL_CLANG_COMPILE := true
 TARGET_KERNEL_HEADER_ARCH := arm64
 TARGET_KERNEL_CONFIG := potter_defconfig
 TARGET_KERNEL_SOURCE := kernel/motorola/potter
-KERNEL_TOOLCHAIN := $(PWD)/prebuilts/gcc/$(HOST_OS)-x86/aarch64/aarch64-linux-android-4.9/bin
 TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-androidkernel-
-TARGET_USE_SDCLANG := true
 
 # Audio
 AUDIO_FEATURE_ENABLED_ALAC_OFFLOAD := true
